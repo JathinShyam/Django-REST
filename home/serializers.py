3062fce59555ff91c46bee4481e0e56835488437
K Shyam Kumar Reddy <kshyamrdy@gmail.com>
2023-11-03 00:09:23 +0530

    added APIView class, ModelViewSet, status code and Token auth

--- home/serializers.py
+++ home/serializers.py
@@ -1,9 +1,80 @@
 from rest_framework import serializers
-from home.models import Person
+from home.models import Person, Color
+from django.contrib.auth.models import User
+
+
+class RegisterSerializer(serializers.Serializer):
+    username = serializers.CharField()
+    email = serializers.EmailField()
+    password = serializers.CharField()
+
+    def validate(self, data):
+            if data['username']:
+                if User.objects.filter(username=data['username']).exists():
+                    raise serializers.ValidationError("Username already exists")
+                
+            if data['email']:
+                if User.objects.filter(email=data['email']).exists():
+                    raise serializers.ValidationError("email already exists")
+                
+            return data
+    
+    def create(self, validated_data):
+        user = User.objects.create(username=validated_data['username'], email=validated_data['email'])
+        user.set_password(validated_data['password'])
+        user.save()
+        print(validated_data)
+        return validated_data
+    
+
+class LoginSerializer(serializers.Serializer):
+    # email = serializers.EmailField()
+    username = serializers.CharField()
+    password = serializers.CharField()
+
+class ColorSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Color
+        fields = ['color_name', 'id']
 
 class PeopleSerializer(serializers.ModelSerializer):
+    # color = ColorSerializer()
+    # color_info = serializers.SerializerMethodField()
     class Meta:
         model = Person
         fields = '__all__'
+        # depth = 1
         # fields = ['name', 'age']
         # exclude = ['name']
+
+    # def validate_age(self, age):
+    #     if age < 18:
+    #         raise serializers.ValidationError("Age must be greater than 18")
+    #     return age
+
+    # def validate_name(self, name):
+    #     special_characters = "~!@#$%^&*()_+|:<>?`-=[]\;',./"
+    #     if any(c in special_characters for c in name):
+    #         raise serializers.ValidationError("Name must not contain special characters")
+    #     return name
+
+    def get_color_info(self, obj):
+        if obj.color is not None:
+            color_obj = Color.objects.get(id=obj.color.id)
+            return {'color_name': color_obj.color_name, 'hex_code': '#000000'}
+        else:
+            return {'color_name': None, 'hex_code': None}
+        
+        # color_obj = Color.objects.get(id = obj.color.id)
+
+        # return ({'color_name' : color_obj.color_name, 'hex_code' : '#000000'})
+
+    def validate(self, data):
+        special_characters = "~!@#$%^&*()_+|:<>?`-=[]\;',./"
+        if any(c in special_characters for c in data['name']):
+            raise serializers.ValidationError("Name must not contain special characters")
+
+
+        # if data['age'] < 18:
+        #     raise serializers.ValidationError("Age must be greater than 18")
+        return data
--- home/views.py
+++ home/views.py
@@ -1,7 +1,108 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from home.models import Person
-from home.serializers import PeopleSerializer
+from home.serializers import PeopleSerializer, LoginSerializer, RegisterSerializer
+from rest_framework.views import APIView
+from rest_framework import viewsets
+from rest_framework import status
+from django.contrib.auth.models import User
+from django.contrib.auth.models import User
+from rest_framework import serializers
+from django.contrib.auth import authenticate
+from rest_framework.authtoken.models import Token
+
+
+class LoginAPI(APIView):
+    def post(self, request):
+        data = request.data
+        serializer = LoginSerializer(data=data)
+        if not serializer.is_valid():
+            return Response({
+                'status' : False,
+                'message' : serializer.errors,
+            }, status=status.HTTP_400_BAD_REQUEST)
+        
+        print(serializer.data)
+        user = authenticate(username=serializer.data['username'], password=serializer.data['password'])
+        print(user)
+        if not user:
+            return Response({
+                'status' : False,
+                'message' : 'Invalid credentials',
+            }, status=status.HTTP_400_BAD_REQUEST)
+        token, _ = Token.objects.get_or_create(user=user)
+        print(token)
+        return Response({
+            'status' : True,
+            'message' : 'User logged in successfully',
+            'token' : str(token)
+        },
+        status=status.HTTP_200_OK)
+
+class RegisterAPI(APIView):
+    def post(self, request):
+        data = request.data
+        serializer = RegisterSerializer(data=data)
+
+        if not serializer.is_valid():
+            return Response({
+                'status' : False,
+                'message' : serializer.errors,
+            }, status=status.HTTP_400_BAD_REQUEST)
+        serializer.save()
+
+        return Response({
+            'status' : True,
+            'message' : 'User created successfully'
+        },
+        status=status.HTTP_201_CREATED)
+
+class PersonAPI(APIView):
+    def get(self, request):
+        objs = Person.objects.filter(color__isnull=False)
+        serializer = PeopleSerializer(objs, many=True)
+        return Response(serializer.data)
+        # return Response({'message' : 'This is a get request'})
+    
+    def post(self, request):
+        data = request.data
+        serializer = PeopleSerializer(data = data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        
+        return Response(serializer.errors)
+        # return Response({'message' : 'This is a post request'})
+    
+    def put(self, request):
+        data = request.data
+        refObj = Person.objects.get(id=data['id'])
+        serializer=PeopleSerializer(instance=refObj , data=request.data)
+        # serializer = PeopleSerializer(data = data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        
+        return Response(serializer.errors)
+        # return Response({'message' : 'This is a put request'})
+    
+    def patch(self, request):
+        data = request.data
+        obj = Person.objects.get(id=data['id'])
+        serializer = PeopleSerializer(obj, data = data, partial=True)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        
+        return Response(serializer.errors)
+        # return Response({'message' : 'This is a patch request'})
+    
+    def delete(self, request):
+        data = request.data
+        obj = Person.objects.get(id=data['id'])
+        obj.delete()
+        return Response({'msg': 'Data deleted successfully'})   
+        # return Response({'message' : 'This is a delete request'})
 
 
 @api_view(['GET', 'POST'])
@@ -23,11 +124,22 @@ def index(request):
         print("POST method")
         return Response(courses)
 
+@api_view(['POST'])
+def login(request):
+    data = request.data
+    serializer = LoginSerializer(data=data)
+    if serializer.is_valid():
+        data = serializer.validated_data
+        print(data)
+        return Response({'message' : 'success'})
+    
+    return Response(serializer.errors)
+
 
 @api_view(['GET', 'POST', 'PUT', 'PATCH', 'DELETE'])
 def person(request):
     if request.method == 'GET':
-        objs = Person.objects.all()
+        objs = Person.objects.filter(color__isnull=False)
         serializer = PeopleSerializer(objs, many=True)
         return Response(serializer.data)
     
@@ -65,4 +177,16 @@ def person(request):
         data = request.data
         obj = Person.objects.get(id=data['id'])
         obj.delete()
-        return Response({'msg': 'Data deleted successfully'})   
+        return Response({'msg': 'Data deleted successfully'}) 
+
+class PeopleViewSet(viewsets.ModelViewSet):
+    serializer_class = PeopleSerializer
+    queryset = Person.objects.all()
+
+    def list(self, request):
+        search = request.GET.get('search')
+        queryset = self.queryset
+        if search is not None:
+            queryset = queryset.filter(name__startswith=search)
+        serializer = PeopleSerializer(queryset, many=True)
+        return Response({'status': 200, 'data' : serializer.data})
